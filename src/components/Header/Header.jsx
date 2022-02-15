d372dcbb58c2dc5a2a87a543563e7f5e5cdf7108
Guilherme Andrade Rosa <gui.rsa@hotmail.com>
2022-02-15 15:02:54 -0300

    requisito 04

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import PropTypes from 'prop-types';
 
-export default class Header extends Component {
+class Header extends Component {
   render() {
+    const { name, gravatarEmail } = this.props;
     return (
       <header>
-        <img src="" alt="profile-img" data-testid="header-profile-picture" />
-        <h2 data-testid="header-player-name">Nome</h2>
+        <img
+          src={ `https://www.gravatar.com/avatar/${md5(gravatarEmail).toString()}` }
+          alt="profile-img"
+          data-testid="header-profile-picture"
+        />
+        <h2 data-testid="header-player-name">{ name }</h2>
         <span data-testid="header-score">0</span>
       </header>
     );
   }
 }
+
+const mapStateToProps = ({ player: { name, gravatarEmail } }) => ({
+  name,
+  gravatarEmail,
+});
+
+export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  email: PropTypes.string,
+  gravatarEmail: PropTypes.string,
+}.isRequired;
