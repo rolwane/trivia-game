d8c4f7fd2f045b098688678d59635414cd88a854
Rolwane <rolwane@hotmail.com>
2022-02-15 17:17:51 -0300

    criaçao do shuffler

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
-class QuestionCard extends React.Component {
+import shuffler from '../../helpers/shuffler';
+
+class QuestionCard extends Component {
   render() {
-    const { data: { category, question, correct_answer } } = this.props;
+    const { data = {} } = this.props;
+    const { category, question, incorrect_answers, correct_answer } = data;
+
+    const shuffleArray = [...incorrect_answers, correct_answer];
+
     return (
       <section>
         <p data-testid="question-category">{category}</p>
         <h3 data-testid="question-text">{question}</h3>
-        {}
       </section>
     );
   }
 }
 
 export default QuestionCard;
+
+QuestionCard.propTypes = {
+  data: propTypes.object,
+}.isRequired;
