5bacf03ee5f490c1de483156ecd7faf3bbd4e488
bruce <brucealberto2@gmail.com>
2022-02-15 17:48:34 -0300

    quase feito

--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
-
 import shuffler from '../../helpers/shuffler';
 
 class QuestionCard extends Component {
   render() {
     const { data = {} } = this.props;
-    const { category, question, incorrect_answers, correct_answer } = data;
+    const {
+      category,
+      question,
+      incorrect_answers: incorrectAnswers = [],
+      correct_answer: correctAnswer,
+    } = data;
 
-    const shuffleArray = [...incorrect_answers, correct_answer];
+    const shuffleArray = shuffler([...incorrectAnswers, correctAnswer]);
 
     return (
       <section>
         <p data-testid="question-category">{category}</p>
         <h3 data-testid="question-text">{question}</h3>
+        <div data-testid="answer-options">
+          {shuffleArray.map((answer, index) => {
+            if (answer === correctAnswer) {
+              return (
+                <button
+                  type="button"
+                  data-testid="correct-answer"
+                  key={ answer }
+                >
+                  {answer}
+
+                </button>);
+            }
+            return (
+              <button
+                type="button"
+                data-testid={ `wrong-answer-${index}` }
+                key={ answer }
+              >
+                {answer}
+
+              </button>);
+          })}
+        </div>
+
       </section>
     );
   }
