e700f810c783460206fb9442a258341684776f66
Rolwane <rolwane@hotmail.com>
2022-02-17 09:43:58 -0300

    Requisito 17

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
-    return <h1 data-testid="ranking-title">Ranking page</h1>;
+    const { history } = this.props;
+
+    return (
+      <>
+        <h1 data-testid="ranking-title">Ranking page</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Inicio
+        </button>
+      </>
+    );
   }
 }
 
 export default Ranking;
+
+Ranking.propTypes = {
+  history: propTypes.object,
+}.idRequired;
